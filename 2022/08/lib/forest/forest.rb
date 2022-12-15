f3a437c1aab2e05b4f5c31cef4ecf766180be221
Nicolás Órdenes <nicordenes@gmail.com>
2022-12-15 19:57:36 -0300

    refactor Forest#check_visibility!
    remove repetition and add #surrounding_trees method that should
    help with the second part.

--- 2022/08/lib/forest/forest.rb
+++ 2022/08/lib/forest/forest.rb
@@ -30,61 +30,33 @@ class Forest
   end
 
   def visible_trees
+    check_visibiliy!
     trees.select(&:visible?)
   end
 
   def check_visibiliy!
-    left_visibility!
-    right_visibility!
-    top_visibility!
-    bottom_visibility!
-  end
+    row_count = @trees.count
+    column_count = @trees.transpose.count
 
-  def left_visibility!
-    @trees.each do |row|
-      max_height = -1
-      row.each do |tree|
-        if tree.height > max_height
-          tree.visible!
-          max_height = tree.height
+    row_count.times do |i|
+      column_count.times do |j|
+        tree = @trees[i][j]
+
+        surrounding_trees(i, j).each do |direction, trees|
+          if trees.empty? || trees.map(&:height).max < tree.height
+            tree.visible!
+          end
         end
       end
     end
   end
 
-  def right_visibility!
-    @trees.each do |row|
-      max_height = -1
-      row.reverse_each do |tree|
-        if tree.height > max_height
-          tree.visible!
-          max_height = tree.height
-        end
-      end
-    end
-  end
-
-  def top_visibility!
-    @trees.transpose.each do |column|
-      max_height = -1
-      column.each do |tree|
-        if tree.height > max_height
-          tree.visible!
-          max_height = tree.height
-        end
-      end
-    end
-  end
-
-  def bottom_visibility!
-    @trees.transpose.each do |column|
-      max_height = -1
-      column.reverse_each do |tree|
-        if tree.height > max_height
-          tree.visible!
-          max_height = tree.height
-        end
-      end
-    end
+  def surrounding_trees(i, j)
+    {
+      left: @trees[i][0...j],
+      right: @trees[i][j + 1..],
+      top: @trees.transpose[j][0...i],
+      bottom: @trees.transpose[j][i + 1..],
+    }
   end
 end
--- 2022/08/spec/forest_spec.rb
+++ 2022/08/spec/forest_spec.rb
@@ -4,31 +4,36 @@ describe Forest do
   describe "#visible_trees" do
     it { expect(forest.visible_trees).to be_an(Array) }
     it do
-      forest.check_visibiliy!
       expect(forest.visible_trees.count).to eq(21)
     end
   end
 
-  describe "#left_visibility" do
-    it "counts 11 trees visible from the left" do
-      forest.left_visibility!
-      expect(forest.visible_trees.count).to eq(11)
+  describe "#surrounding_trees" do
+    it "works on a corner tree" do
+      foo = forest.surrounding_trees(0, 0)
+
+      expect(foo[:left].map(&:height)).to eq([])
+      expect(foo[:right].map(&:height)).to eq([0, 3, 7, 3])
+      expect(foo[:top].map(&:height)).to eq([])
+      expect(foo[:bottom].map(&:height)).to eq([2, 6, 3, 3])
     end
 
-    it "counts 11 trees visible from the right" do
-      forest.right_visibility!
-      p forest.trees.map(&:visible?)
-      expect(forest.visible_trees.count).to eq(11)
+    it "works on a wall tree" do
+      foo = forest.surrounding_trees(4, 2)
+
+      expect(foo[:left].map(&:height)).to eq([3, 5])
+      expect(foo[:right].map(&:height)).to eq([9, 0])
+      expect(foo[:top].map(&:height)).to eq([3, 5, 3, 5])
+      expect(foo[:bottom].map(&:height)).to eq([])
     end
 
-    it "counts 10 trees visible from the top" do
-      forest.top_visibility!
-      expect(forest.visible_trees.count).to eq(10)
-    end
+    it "works on a middle tree" do
+      foo = forest.surrounding_trees(3, 1)
 
-    it "counts 8 trees visible from the bottom" do
-      forest.bottom_visibility!
-      expect(forest.visible_trees.count).to eq(8)
+      expect(foo[:left].map(&:height)).to eq([3])
+      expect(foo[:right].map(&:height)).to eq([5, 4, 9])
+      expect(foo[:top].map(&:height)).to eq([0, 5, 5])
+      expect(foo[:bottom].map(&:height)).to eq([5])
     end
   end
 end
