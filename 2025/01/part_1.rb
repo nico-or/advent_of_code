da5bb8113d2f8cd153a53fb6bc9ba1f74e967c3b
nicolas <nicordenes@gmail.com>
2025-12-01 17:28:18 -0300

    add 2025-01 solution

---
+++ 2025/01/part_1.rb
@@ -0,0 +1,28 @@
+DIR_DICT = { 'L' => -1, 'R' => 1 }
+START_POS = 50
+POS_COUNT = 100
+
+
+def parse_cmd(cmd)
+  dir = cmd[0]
+  num = cmd[1..]
+
+  DIR_DICT.fetch(dir) * num.to_i
+end
+
+filename = ARGV[0]
+
+input = File.readlines(filename, chomp: true)
+
+moves = input.map { parse_cmd(it) }
+
+# warn: mutation
+curr_pos = START_POS
+password = 0
+
+moves.each do |move|
+  curr_pos = (curr_pos + move).modulo(POS_COUNT)
+  password += 1 if curr_pos.zero?
+end
+
+puts password
---
+++ 2025/01/part_2.rb
@@ -0,0 +1,44 @@
+DIR_DICT = { 'L' => -1, 'R' => 1 }
+START_POS = 50
+POS_COUNT = 100
+
+def parse_cmd(cmd)
+  dir = cmd[0]
+  num = cmd[1..]
+
+  DIR_DICT.fetch(dir) * num.to_i
+end
+
+filename = ARGV[0]
+input = File.readlines(filename, chomp: true)
+moves = input.map { parse_cmd(it) }
+
+# warn: mutation
+curr_pos = START_POS
+password = 0
+
+moves.each do |move|
+  
+  old_pos = curr_pos
+  new_pos = curr_pos + move 
+  curr_pos = new_pos.modulo(POS_COUNT)
+  
+  laps = new_pos.div(POS_COUNT)
+
+  password += laps.abs
+  password += 1 if curr_pos.zero?
+
+  # arrived at this by testing, but why?
+  password -= 1 if curr_pos.zero? && new_pos >= 100
+  password -= 1 if old_pos.zero? && move.negative? 
+  
+#  puts <<~MSG
+#     old: #{old_pos}
+#     move: #{move}
+#     new: #{new_pos}/#{curr_pos}
+#     pwd: #{password}
+#     {'-'*40}
+#   MSG
+end
+
+puts password
