5804d4d2ccc039cd6753f0953438746f8c65ea27
nicolas <nicordenes@gmail.com>
2025-12-11 13:38:17 -0300

    add 2025-11 solution

---
+++ 2025/11/part_1.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+filename = ARGV[0]
+
+input = File.read(filename).lines(chomp: true)
+
+GRAPH = Hash.new { |h, k| h[k] = [] }
+
+input.each do |line|
+  node, targets = line.split(': ')
+
+  targets.split.each do |target|
+    GRAPH[node] << target
+  end
+end
+
+def dfs(node, target, graph, count = 0, memo = {})
+  return memo[node] if memo.key?(node) # already visited
+  return memo[node] = 1 if node == target # good leaf
+  return memo[node] = 0 unless graph.key?(node) # bad leaf
+
+  memo[node] = count + graph[node].sum { |child| dfs(child, target, graph, count, memo) }
+end
+
+START_NODE = ARGV[1] || 'you'
+END_NODE = 'out'
+
+p dfs(START_NODE, END_NODE, GRAPH)
